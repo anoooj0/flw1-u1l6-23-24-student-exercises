c791ca5a21923fb1a7ff6a053fc7a663ff8298f5
anoooj0 <130328624+anoooj0@users.noreply.github.com>
2023-11-08 23:23:43 +0000

    DOM manipulation

--- updateImage.js
+++ updateImage.js
@@ -10,7 +10,7 @@ buttonOne.addEventListener('click', function() {
     // 1. Select the placeholder and update the src.
     // - The image address should be a picture of a place you want to travel to.
     // - Use Google Search, find a photo you want, right-click, and copy the image address.
-
+    placeholder.src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg/278px-La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg";
 
 
 
@@ -24,7 +24,7 @@ buttonTwo.addEventListener('click', function() {
     // - The image address should be a picture that represents your mood.
     // - Use Google Search, find a photo you want, right-click, and copy the image address.
 
-
+  placeholder.src="https://www.nkch.org/blog/_assets/images/2023/03-march/tiredheader.jpg"
 
     
 
